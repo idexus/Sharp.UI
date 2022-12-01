8d15d89152b84608ddc85438b8148b8810651004
Pawel Krzywdzinski <pawel@dexus.pl>
2022-12-02 00:36:39 +0100

    update

--- Sharp.UI/src/Triggers/DataTrigger.cs
+++ Sharp.UI/src/Triggers/DataTrigger.cs
@@ -1,6 +1,5 @@
 ﻿namespace Sharp.UI
 {
-
     public class DataTrigger<T> : DataTrigger
     {
         public DataTrigger(Func<Binding, Binding> bindingBuilder, object value) : base(new Microsoft.Maui.Controls.DataTrigger(typeof(T)))
