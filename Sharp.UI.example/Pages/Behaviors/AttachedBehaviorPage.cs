a24d34ec79fbc6352a333d150d06665622a2a72e
Pawel Krzywdzinski <pawel@dexus.pl>
2022-12-03 21:08:38 +0100

    add files

---
+++ Sharp.UI.example/Pages/Behaviors/AttachedBehaviorPage.cs
@@ -0,0 +1,40 @@
+﻿namespace Sharp.UI.Example;
+
+
+public class NumericValidationBehavior : Behavior<Entry>
+{
+    protected override void OnAttachedTo(Entry entry)
+    {
+        entry.TextChanged += OnEntryTextChanged;
+        base.OnAttachedTo(entry);
+    }
+
+    protected override void OnDetachingFrom(Entry entry)
+    {
+        entry.TextChanged -= OnEntryTextChanged;
+        base.OnDetachingFrom(entry);
+    }
+
+    void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+    {
+        double result;
+        bool isValid = double.TryParse(args.NewTextValue, out result);
+        ((Entry)sender).TextColor = isValid ? Colors.Green : Colors.Red;
+    }
+}
+
+public class BehaviorTestPage : ContentPage
+{
+	public BehaviorTestPage()
+	{
+		Content = new VStack
+		{
+			new Entry("Enter text...", out var entry).Text("")
+                .Behaviors(new Behavior[]
+                {
+                    new NumericValidationBehavior()
+                })
+		}
+		.VerticalOptions(LayoutOptions.Center);
+	}
+}
---
+++ Sharp.UI.example/Pages/Behaviors/BehaviorTestPage.cs
@@ -0,0 +1,44 @@
+﻿namespace Sharp.UI.Example;
+
+
+public class NumericValidationBehavior : Behavior<Entry>
+{
+    protected override void OnAttachedTo(Entry entry)
+    {
+        entry.TextChanged += OnEntryTextChanged;
+        base.OnAttachedTo(entry);
+    }
+
+    protected override void OnDetachingFrom(Entry entry)
+    {
+        entry.TextChanged -= OnEntryTextChanged;
+        base.OnDetachingFrom(entry);
+    }
+
+    void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+    {
+        double result;
+        bool isValid = double.TryParse(args.NewTextValue, out result);
+        ((Entry)sender).TextColor = isValid ? Colors.Green : Colors.Red;
+    }
+}
+
+public class BehaviorTestPage : ContentPage
+{
+	public BehaviorTestPage()
+	{
+        Resources = new ResourceDictionary
+        {
+            new Style<Entry>
+            {
+                Entry.BehaviorsProperty.Set(new Behavior[] {new NumericValidationBehavior()})
+            }
+        };
+
+		Content = new VStack
+		{
+			new Entry("Enter text...", out var entry).Text("")
+		}
+		.VerticalOptions(LayoutOptions.Center);
+	}
+}
