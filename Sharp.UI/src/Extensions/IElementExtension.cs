83a4d27dfe2e6787679191296291daf3aa991723
Pawel Krzywdzinski <pawel@dexus.pl>
2022-12-03 22:48:34 +0100

    ContextFlyout

---
+++ Sharp.UI/src/Extensions/IElementExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Sharp.UI
+{
+	public static class IElementExtension
+	{
+        public static T ContextFlyout<T>(this T obj,
+            Microsoft.Maui.Controls.MenuFlyout menuFlyout)
+            where T : IElement
+        {
+            var mauiObject = MauiWrapper.GetObject<Microsoft.Maui.Controls.Element>(obj);
+            mauiObject.SetValue(FlyoutBase.ContextFlyoutProperty, menuFlyout);
+            return obj;
+        }
+    }
+}
+
