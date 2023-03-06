c4fc6443ebbf0f109826f3190008b971ed209c68
idexus <pawel@dexus.pl>
2023-03-06 19:57:41 +0100

    example update

--- src/samples/ExampleApp/App.cs
+++ src/samples/ExampleApp/App.cs
@@ -9,36 +9,34 @@ public partial class App : Application
 {
     public App()
     {
-        MainPage = ActivatorUtilities.GetServiceOrCreateInstance<TestPage>(Application.Services);
+        MainPage = new ContentPage(e => e.Resources(AppResources.Default))
+        {
+            new VStack(e => e.CenterVertically())
+            {
+                new Button("Single Page")
+                    .Margin(10)
+                    .WidthRequest(200)
+                    .OnClicked(b =>
+                    {
+                        MainPage = ActivatorUtilities.GetServiceOrCreateInstance<TestPage>(Application.Services);
+                    }),
 
-        //MainPage = new ContentPage(e => e.Resources(AppResources.Default))
-        //{
-        //    new VStack(e => e.CenterVertically())
-        //    {
-        //        new Button("Single Page")
-        //            .Margin(10)
-        //            .WidthRequest(200)
-        //            .OnClicked(b =>
-        //            {
-        //                MainPage = ActivatorUtilities.GetServiceOrCreateInstance<TestPage>(Application.Services);
-        //            }),
+                new Button("Flyout Application")
+                    .Margin(10)
+                    .WidthRequest(200)
+                    .OnClicked(b =>
+                    {
+                        MainPage = ActivatorUtilities.GetServiceOrCreateInstance<FlyoutMainPage>(Application.Services);
+                    }),
 
-        //        new Button("Flyout Application")
-        //            .Margin(10)
-        //            .WidthRequest(200)
-        //            .OnClicked(b =>
-        //            {
-        //                MainPage = ActivatorUtilities.GetServiceOrCreateInstance<FlyoutMainPage>(Application.Services);
-        //            }),
-
-        //        new Button("Shell Application")
-        //            .Margin(10)
-        //            .WidthRequest(200)
-        //            .OnClicked(b =>
-        //            {
-        //                MainPage = new AppShell();
-        //            }),
-        //    }
-        //};
+                new Button("Shell Application")
+                    .Margin(10)
+                    .WidthRequest(200)
+                    .OnClicked(b =>
+                    {
+                        MainPage = new AppShell();
+                    }),
+            }
+        };
     }
 }
